2527d5a3a8a33ab5fe2d42f834308c7da72906d0
sanketkesarkar <145060601+sanketkesarkar@users.noreply.github.com>
2023-11-27 18:38:57 -0500

    Update dml.sql
    Insert Values into Brand, Part,  Manufacturer, AssemblyUnit, Batch, Cartype

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -62,3 +62,131 @@ INSERT INTO [dbo].[Supplier] ([SupplierName], [location_id]) VALUES
 ('Supplier E', 5),
 ('Supplier F', 6),
 ('Supplier G', 7);
+
+
+
+
+INSERT INTO [dbo].[Brand] ([BrandName],[location_id]) VALUES 
+('GM Parts', 7),
+('Car Care',8),
+('AM Part', 9),
+('Central Part', 10),
+('3M Products', 11);
+
+INSERT INTO [dbo].[Part] ([PartName], [description], [brand_id]) VALUES 
+('GM-Engine', 'The engine block features parts such as the timing chain, camshaft, crankshaft, spark plugs, cylinder heads, valves and pistons.',1),
+('3M-Engine', 'The engine block features parts such as the timing chain, camshaft, crankshaft', 5),
+('CentalPart-Engine', 'The engine block features parts such as the timing chain, camshaft, crankshaft', 4),
+
+('3M-Transmission','The transmission is a gearbox filled with gears and gear trains that makes effective use of the engines torque to change the gears and power the vehicle.', 5),
+('GM-Transmission','GM transmission is a gearbox filled with gears and gear trains that makes effective use of the engines torque to change the gears and power the vehicle.', 1),
+('AmPart-Transmission','AM Part transmission is a gearbox filled with gears and gear trains that makes effective use of the engines torque to change the gears and power the vehicle.', 3),
+
+('CarCare-Battery','The battery delivers the electricity needed to run your vehicles electrical components. Without a battery, your car wont run.', 2),
+('3M-Battery','3M battery delivers the electricity needed to run your vehicles electrical components. Without a battery, your car wont run.', 5),
+('CentalPart-Battery','CentalPart battery delivers the electricity needed to run your vehicles electrical components. Without a battery, your car wont run.', 4),
+
+('AmPart-Alternator','Part of the electrical system, the alternator charges the battery and powers the electrical system while your car is running.', 3),
+('CentalPart-Alternator','Central Part of the electrical system, the alternator charges the battery and powers the electrical system while your car is running.', 4),
+('GM-Alternator','GM Part of the electrical system, the alternator charges the battery and powers the electrical system while your car is running.', 1),
+
+('CentalPart-Radiator','The radiator is responsible for helping the engine keep cool by removing heat from coolant before it is pumped back through the engine.', 4),
+('AMPart-Radiator','The radiator is responsible for helping the engine keep cool by removing heat from coolant before it is pumped back through the engine.', 3),
+('3M-Radiator','The radiator is responsible for helping the engine keep cool by removing heat from coolant before it is pumped back through the engine.', 5),
+
+('3M-Brake','3M brakes are one of the most important safety systems on your vehicle', 5),
+('CarCare-Brake','Car Care brakes are one of the most important safety systems on your vehicle', 2),
+('CentalPart-Brake','Central Part brakes are one of the most important safety systems on your vehicle', 4);
+
+
+
+INSERT INTO [dbo].[Manufacturer] ([ManufacturerName],[location_id]) VALUES 
+('Manufacturer-1',12),
+('Manufacturer-2',13),
+('Manufacturer-3',14);
+
+
+INSERT INTO [dbo].[AssemblyUnit] ([AssemblyUnitName], [brand_id], [location_id]) VALUES 
+('Assembly Unit 1', 1, 7),
+('Assembly Unit 2', 2, 8),
+('Assembly Unit 3', 3, 9),
+('Assembly Unit 4', 4, 10),
+('Assembly Unit 5', 5, 11);
+
+
+INSERT INTO [dbo].[Batch] ([cost], [manufacturer_id], [part_id]) VALUES 
+(1000, 1, 2),
+(1100, 1, 3),
+(1200, 1, 4),
+(1300, 1, 5),
+(1400, 1, 6),
+(1500, 1, 7),
+(1600, 1, 8),
+(1700, 1, 9),
+(1800, 1, 10),
+(1900, 1, 11),
+(2000, 1, 12),
+(2100, 1, 13),
+(2200, 1, 14),
+(2300, 1, 15),
+(2400, 1, 16),
+(2500, 1, 17),
+(2600, 1, 18),
+(2700, 1, 19),
+
+(1000, 2, 2),
+(1100, 2, 3),
+(1200, 2, 4),
+(1300, 2, 5),
+(1400, 2, 6),
+(1500, 2, 7),
+(1600, 2, 8),
+(1700, 2, 9),
+(1800, 2, 10),
+(1900, 2, 11),
+(2000, 2, 12),
+(2100, 2, 13),
+(2200, 2, 14),
+(2300, 2, 15),
+(2400, 2, 16),
+(2500, 2, 17),
+(2600, 2, 18),
+(2700, 2, 19),
+
+(1000, 3, 2),
+(1100, 3, 3),
+(1200, 3, 4),
+(1300, 3, 5),
+(1400, 3, 6),
+(1500, 3, 7),
+(1600, 3, 8),
+(1700, 3, 9),
+(1800, 3, 10),
+(1900, 3, 11),
+(2000, 3, 12),
+(2100, 3, 13),
+(2200, 3, 14),
+(2300, 3, 15),
+(2400, 3, 16),
+(2500, 3, 17),
+(2600, 3, 18),
+(2700, 3, 19);
+
+
+
+
+INSERT INTO [dbo].[CarType] ([CarTypeName], [description], [is_electric]) VALUES 
+('Sedan','Medium size vehicle',0),
+('Sedan','Medium size vehicle',1),
+('SUV', 'Big size Vehicle',0),
+('SUV', 'Big size Vehicle',1),
+('Hatchback', 'small size Vehicle',0),
+('Hatchback', 'small size Vehicle',1),
+('Truck', 'heavy Vehicle',0),
+('Truck', 'heavy Vehicle',1),
+('Bike', 'two wheeler',0),
+('Bike', 'two wheeler',1),
+('Bus', 'public transport',0),
+('Bus', 'public transport',1);
+
+
