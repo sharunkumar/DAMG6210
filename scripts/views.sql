fccfae2de339fa791d68ad223b1ab08e7317c816
Sharun <sharunksplus@gmail.com>
2023-11-28 20:03:04 -0500

    fix and format views

--- scripts/views.sql
+++ scripts/views.sql
@@ -27,15 +27,19 @@ as
 GO
 -- reporting related views
 GO
-create or alter view sales_by_parts as select part_id, p.PartName, p.[description], sum(total_price) [Total_Sales]
-from [Transaction] t left join TransactionRow r on t.TransactionID = r.transaction_id and transaction_type = 'S'
-    left join Batch b on r.part_batch_id = b.BatchID
-    left join Part p on p.PartID = b.BatchID
-group by part_id, p
+create or alter view sales_by_parts
+as
+    select part_id, p.PartName, p.[description], sum(total_price) [Total_Sales]
+    from [Transaction] t inner join TransactionRow r on t.TransactionID = r.transaction_id and transaction_type = 'S'
+        inner join Batch b on r.part_batch_id = b.BatchID
+        inner join Part p on p.PartID = b.BatchID
+    group by part_id, p
 .PartName, p.[description]
 GO
-create or alter view transaction_logistics as 
-select dbo.calc_distance(f.latitude, f.longitude, t.latitude, t.longitude) [TransportationDistance], [transaction].* from [Transaction] left join [Location] [f] on [Transaction].from_location_id = f.LocationID
-left join [Location] [t] on [Transaction].to_location_id = t.LocationID
+create or alter view transaction_logistics
+as
+    select dbo.calc_distance(f.latitude, f.longitude, t.latitude, t.longitude) [TransportationDistance], [transaction].*
+    from [Transaction] left join [Location] [f] on [Transaction].from_location_id = f.LocationID
+        left join [Location] [t] on [Transaction].to_location_id = t.LocationID
 go
 -- select * from transaction_logistics
