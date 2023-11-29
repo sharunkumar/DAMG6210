b851b2d277282bea1474dde640adce31cb9a144f
Sharun <sharunksplus@gmail.com>
2023-11-28 20:54:46 -0500

    seed function to generate random transaction data

--- gui/prisma/seed.ts
+++ gui/prisma/seed.ts
@@ -1,39 +1,65 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 async function main() {
-  const locations = await prisma.location.findMany()
-  const users = await prisma.user.findMany()
-  const transports = await prisma.transport.findMany()
-  const transaction = await prisma.transaction.create({
-    data: {
-      from_location_id: locations[0].LocationID,
-      to_location_id: locations[0].LocationID,
-      transaction_type: "S",
-      created_user_id: users[0].UserID,
-      updated_user_id: users[0].UserID,
-      transported_by_id: transports[0].TransportID,
-    }
-  })
+  let rng = Math.random();
+  const locations = await prisma.location.findMany();
+  const users = await prisma.user.findMany();
+  const transports = await prisma.transport.findMany();
 
-  const batches = await prisma.batch.findMany()
+  // const parts = await prisma.part.findMany();
 
-  for (let i = 0; i < 10; i++) {
-    await prisma.transactionRow.createMany({
-      data: {
-        part_batch_id:batches[0].BatchID,
-         quantity: 1,transaction_id: transaction.TransactionID,
-         unit_price: 100
+  const runningDate = new Date("2023-01-01");
+
+  const today = new Date();
+
+  while (runningDate < today) {
+    console.log({ runningDate });
+    // random 10 transactions per day
+    for (let i = 0; i < 10; i++) {
+      const transaction = await prisma.transaction.create({
+        data: {
+          from_location_id: needle(locations).LocationID,
+          to_location_id: needle(locations).LocationID,
+          transaction_type: "S",
+          created_user_id: needle(users).UserID,
+          updated_user_id: needle(users).UserID,
+          transported_by_id: needle(transports).TransportID,
+          created_date: runningDate,
+        },
+      });
+      const batches = await prisma.batch.findMany();
+      for (let i = 0; i < 10; i++) {
+        const batch = needle(batches);
+        await prisma.transactionRow.createMany({
+          data: {
+            part_batch_id: batch.BatchID,
+            quantity: rand(20),
+            transaction_id: transaction.TransactionID,
+            unit_price: batch.cost,
+          },
+        });
       }
-    })
-  }
+    }
 
+    runningDate.setDate(runningDate.getDate() + 1);
+  }
 }
+function rand(n: number): number {
+  // Generate a random number between 1 and n (inclusive)
+  return Math.floor(Math.random() * n) + 1;
+}
+
+function needle<T>(haystack: Array<T>): T {
+  const n = haystack.length;
+  return haystack[rand(n) - 1];
+}
+
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
