e0ed061534b67e562ece0e976034f4831562f4c5
Sharun <sharunksplus@gmail.com>
2023-12-10 18:10:16 -0500

    add script to bootstrap the database

---
+++ scripts/bootstrap.ps1
@@ -0,0 +1,7 @@
+SQLCMD.EXE -E -i .\drop.sql
+SQLCMD.EXE -E -i .\ddl.sql
+SQLCMD.EXE -E -i .\triggers.sql
+SQLCMD.EXE -E -i .\procs.sql
+SQLCMD.EXE -E -i .\dml.sql
+SQLCMD.EXE -E -i .\encryption.sql
+SQLCMD.EXE -E -i .\views.sql
